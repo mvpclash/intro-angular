308a2d56f88bc0ec84fb33dc179fd8cf569a59ed
mvpclash <roy3r91@gmail.com>
2024-07-22 08:58:28 -0600

    Se agregó el sidebar, el formulario, las validaciones del mismo, y se configuro para que las variables no se inicializaran en el tsconfig.json

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import JsonPlaceHolderModel from 'src/app/models/jsonPlaceHolder.model';
 import { JsonPlaceHolderServiceService } from 'src/app/services/json-place-holder-service.service';
 
@@ -9,21 +9,39 @@ import { JsonPlaceHolderServiceService } from 'src/app/services/json-place-holde
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+ 
+  loginForm! : FormGroup; 
+  
   public posts: JsonPlaceHolderModel[] = [] ;
-  // loginForm: FormGroup;
+  
 
-  constructor(private jsonPlaceHolderService: JsonPlaceHolderServiceService,
-    private formBuilder : FormBuilder
-    ) {
-      // this.loginForm = this
-     }
+  constructor(
+    private formBuilder : FormBuilder,
+    private jsonPlaceHolderService: JsonPlaceHolderServiceService
+    ) {}
+    
 
   ngOnInit(): void {
+    this.crearFormulario();
     this.jsonPlaceHolderService.obtenerDatos().subscribe(
       (data) => {
         this.posts = data;
       }
     );
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      return alert('Ha hecho login');
+    }
+    return alert('Email o contraseña no cumple con las validaciones');
+  }
+
+  crearFormulario(){
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email] ] ,
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
   }
 
 }
